a2350563fa7e8b6f66351505378a430ee59314b6
Fodor Szilárd <szilard.l.fodor@gmail.com>
2015-03-26 07:53:37 +0100

    first

--- app/src/main/java/hu/example/fodorsz/myfirstapp/MyActivity.java
+++ app/src/main/java/hu/example/fodorsz/myfirstapp/MyActivity.java
@@ -17,7 +17,7 @@ public class MyActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present!
         getMenuInflater().inflate(R.menu.menu_my, menu);
         return true;
     }
