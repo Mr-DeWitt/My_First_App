51f3297f23db60cb03350c9edf95547fb1092315
Fodor Szilárd <szilard.l.fodor@gmail.com>
2015-03-27 07:22:35 +0100

    2 activities

--- app/src/main/java/hu/example/fodorsz/myfirstapp/MyActivity.java
+++ app/src/main/java/hu/example/fodorsz/myfirstapp/MyActivity.java
@@ -1,12 +1,19 @@
 package hu.example.fodorsz.myfirstapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    public static final String EXTRA_MESSAGE = "hu.example.fodorsz.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +37,35 @@ public class MyActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_search:
+                searchClicked();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private AlertDialog searchClicked() {
+        return new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.dialog_title_search))
+                .setMessage(getString(R.string.dialog_message_search))
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .show();
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
